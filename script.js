61914af93363ae3c4c0c684f3c91d8e3f3434fe7
Paulinap3462 <43858710+Paulinap3462@users.noreply.github.com>
2019-10-07 09:37:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Paulina T. Priftakis"
+let userName = "paulinap3462";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
